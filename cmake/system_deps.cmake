e260a612ebcd7329ffa608946390e25d9cf218fd
Mizux Seiha <mizux.dev@gmail.com>
2025-10-07 16:31:58 +0200

    cmake: rework

--- cmake/system_deps.cmake
+++ cmake/system_deps.cmake
@@ -8,23 +8,30 @@ find_package(Threads REQUIRED)
 # distro package already provide a CMake config file...
 set(CMAKE_FIND_PACKAGE_PREFER_CONFIG TRUE)
 
+# libprotobuf force us to depends on ZLIB::ZLIB target
+if(NOT BUILD_ZLIB AND NOT TARGET ZLIB::ZLIB)
+ find_package(ZLIB REQUIRED)
+endif()
+
+if(NOT BUILD_absl AND NOT TARGET absl::base)
+  find_package(absl REQUIRED)
+endif()
+
 if(BUILD_TESTING)
-  if(NOT BUILD_googletest)
-    find_package(googletest REQUIRED)
+  if(NOT BUILD_re2 AND NOT TARGET re2::re2)
+    find_package(re2 REQUIRED)
   endif()
-  if(NOT TARGET GTest::gtest)
-    message(FATAL_ERROR "Target GTest::gtest not available.")
-  endif()
-  if(NOT TARGET GTest::gtest_main)
-    message(FATAL_ERROR "Target GTest::gtest_main not available.")
+
+  if(NOT BUILD_googletest AND NOT TARGET GTest::gtest_main)
+    find_package(GTest REQUIRED)
   endif()
 endif()
 
 # Check language Dependencies
-if(NOT BUILD_pybind11)
+if(NOT BUILD_pybind11 AND NOT TARGET pybind11::pybind11_headers)
   find_package(pybind11 REQUIRED)
 endif()
 
-if(NOT BUILD_pybind11_abseil)
+if(NOT BUILD_pybind11_abseil AND NOT TARGET pybind11_abseil::absl_casters)
   find_package(pybind11_abseil REQUIRED)
 endif()
