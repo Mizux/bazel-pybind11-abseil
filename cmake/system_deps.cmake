7c8e1e97cb70c644b044118760968c55263351ff
Corentin Le Molgat <corentinl@google.com>
2026-07-22 11:06:25 +0200

    cleanup

--- cmake/system_deps.cmake
+++ cmake/system_deps.cmake
@@ -17,6 +17,7 @@ if(NOT BUILD_absl AND NOT TARGET absl::base)
   find_package(absl REQUIRED)
 endif()
 
+# CXX Test
 if(BUILD_TESTING)
   if(NOT BUILD_re2 AND NOT TARGET re2::re2)
     find_package(re2 REQUIRED)
